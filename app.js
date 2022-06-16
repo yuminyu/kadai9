967866e4f6bcae68276a71bf87d210c0a7b34436
yuminyu <102504368+yuminyu@users.noreply.github.com>
2022-06-17 01:38:26 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const crypto = require('crypto');
+const app  = require("express")();
+const http = require("http").createServer(app);
+const io   = require("socket.io")(http);
+
+// HTMLやJSなどを配置するディレクトリ
+const DOCUMENT_ROOT = __dirname + "/public";
+
+/**
+ * "/"にアクセスがあったらindex.htmlを返却
+ */
+app.get("/", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/index.html");
+});
+app.get("/:file", (req, res)=>{
+  res.sendFile(DOCUMENT_ROOT + "/" + req.params.file);
+});
+
+
+/**
+ * [イベント] ユーザーが接続
+ */
+io.on("connection", (socket)=>{
+  console.log("ユーザーが接続しました");
+
+  //---------------------------------
+  // ログイン
+  //---------------------------------
+  (()=>{
+    // トークンを作成
+    const token = makeToken(socket.id);
+    console.log(token);
+
+    // 本人にトークンを送付
+    io.to(socket.id).emit("token", {token:token});
+  })();
+
+  //---------------------------------
+  // 発言を全員に送信
+  //---------------------------------
+  socket.on("post", (msg)=>{
+    io.emit("member-post", msg);
+  });
+});
+
+/**
+ * 3000番でサーバを起動する
+ */
+http.listen(3000, ()=>{
+  console.log("listening on *:3000");
+});
+
+
+
+function makeToken(id){
+  const str = "aqwsedrftgyhujiko" + id;
+  return( crypto.createHash("sha1").update(str).digest('hex') );
+}
